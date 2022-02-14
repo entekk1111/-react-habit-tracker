571c965623c2759c744b783382fb6f116189c4d6
entek <entek@DESKTOP-1QIEOOE>
2022-02-14 15:07:58 +0900

    add habit-tracker

---
+++ src/components/habit.jsx
@@ -0,0 +1,47 @@
+import React, { PureComponent } from "react";
+
+class Habit extends PureComponent {
+  //1.자체적인 상태 state는 없고
+  handleIncreament = () => {
+    this.props.onIncrement(this.props.habit);
+  };
+
+  handleDecrement = () => {
+    this.props.onDecrement(this.props.habit);
+  };
+
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit);
+  };
+
+  render() {
+    //2. 외부에서 전달받은 props 데이터를 보여주기만 하는 컴포넌트
+    const { name, count } = this.props.habit;
+    return (
+      <li className="habit">
+        <span className="habit-name">{name}</span>
+        <span className="habit-count">{count}</span>
+        <button
+          className="habit-button habit-increase"
+          onClick={this.handleIncreament}
+        >
+          <i className="fas fa-plus-square"></i>
+        </button>
+        <button
+          className="habit-button habit-decrease"
+          onClick={this.handleDecrement}
+        >
+          <i className="fas fa-minus-square"></i>
+        </button>
+        <button
+          className="habit-button habit-delete"
+          onClick={this.handleDelete}
+        >
+          <i className="fas fa-trash"></i>
+        </button>
+      </li>
+    );
+  }
+}
+
+export default Habit;
